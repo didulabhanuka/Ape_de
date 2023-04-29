2a97f27aaf911225d8fa47d3baedd23fbfb4d592
didulabhanuka <bhanukadidula78@gmail.com>
2023-04-29 16:19:50 +0530

    category branch test

---
+++ app/src/main/java/com/example/ape_de/Cart_view.kt
@@ -0,0 +1,16 @@
+package com.example.category
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class Cart_view : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart_view)
+    }
+
+}
+
