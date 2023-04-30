b733e06b2c40c05b4c6693beb61bbc0c14ba6db4
Jayani000 <127921420+Jayani000@users.noreply.github.com>
2023-04-30 15:28:57 +0530

    Item branch added

--- app/src/main/java/com/example/ape_de/CustomerViewActivity.kt
+++ app/src/main/java/com/example/ape_de/CustomerViewActivity.kt
@@ -3,9 +3,9 @@ package com.example.ape_de
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CustomerViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_hello_user)
+        setContentView(R.layout.activity_customer_view)
     }
 }
--- app/src/main/java/com/example/ape_de/ViewShopItemActivity.kt
+++ app/src/main/java/com/example/ape_de/ViewShopItemActivity.kt
@@ -3,9 +3,9 @@ package com.example.ape_de
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ViewShopItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_hello_user)
+        setContentView(R.layout.activity_view_shop_item)
     }
 }
